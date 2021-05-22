48fdaa702de17a501b2b5e135d03a4d0a9c72576
Artur Cardoso - TI <arturpaulino@gmail.com>
2021-05-22 11:27:06 -0300

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, {useState} from "react";
+import {Alert} from "react-native";
 
-import { Header } from '../components/Header';
-import { MyTasksList } from '../components/MyTasksList';
-import { TodoInput } from '../components/TodoInput';
+import {Header} from "../components/Header";
+import {MyTasksList} from "../components/MyTasksList";
+import {TodoInput} from "../components/TodoInput";
 
 interface Task {
   id: number;
@@ -11,31 +12,61 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task if it's not empty
+    if (
+      newTaskTitle.toString() == "" ||
+      newTaskTitle == null ||
+      newTaskTitle == undefined
+    ) {
+      Alert.alert("Por favor informe um nome para Tarefa");
+      return false;
+    }
+    const filter = tasks.filter((item) => {
+      return item.title.toLowerCase() == newTaskTitle.toLowerCase()
+        ? true
+        : false;
+    });
+    if (filter.length > 0) {
+      Alert.alert("Tareja ja existe, por favor informE outra tarefa ");
+      return false;
+    }
+    const newItem: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+    setTasks((oldTasks: Task[]) => [...oldTasks, newItem]);
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    const filter = tasks.filter((item) => {
+      if (item.id == id) item.done = !item.done;
+      return true;
+    });
+    setTasks((oldTasks: Task[]) => filter);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const filter = tasks.filter((item) => {
+      if (item.id == id) return false;
+      else return true;
+    });
+    setTasks((oldTasks: Task[]) => filter);
   }
 
   return (
     <>
       <Header />
-
       <TodoInput addTask={handleAddTask} />
 
-      <MyTasksList 
-        tasks={tasks} 
-        onPress={handleMarkTaskAsDone} 
-        onLongPress={handleRemoveTask} 
+      <MyTasksList
+        tasks={tasks}
+        onPress={handleMarkTaskAsDone}
+        onLongPress={handleRemoveTask}
       />
     </>
-  )
+  );
 }
